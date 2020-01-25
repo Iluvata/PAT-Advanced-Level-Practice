308de0511a14fc4701c61f83307507a50da4a296
justin <1124026683@qq.com>
2020-01-25 16:27:55 +0800

    1054 the dominant color

---
+++ 1054_The_Dominant_Color/1054_The_Dominant_Color/1054_The_Dominant_Color.cpp
@@ -0,0 +1,29 @@
+﻿// 1054_The_Dominant_Color.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int m, n;
+	cin >> m >> n;
+	map<int, int> colors;
+	int c;
+	for (int i = 0; i < m * n; ++i) {
+		cin >> c;
+		colors[c]++;
+	}
+	int maxnum = 0;
+	int maxc = 0;
+	for (auto i = colors.begin(); i != colors.end(); ++i) {
+		if (i->second > maxnum) {
+			maxc = i->first;
+			maxnum = i->second;
+		}
+	}
+	cout << maxc;
+}
