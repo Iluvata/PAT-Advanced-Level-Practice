2f0dc56d863062836dc88ca5b58ef9b7ab2a7501
justin <1124026683@qq.com>
2020-01-28 15:46:33 +0800

    1059-1061

---
+++ 1059_Prime_Factors/1059_Prime_Factors/1059_Prime_Factors.cpp
@@ -0,0 +1,52 @@
+﻿// 1059_Prime_Factors.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> isprime(100000, true);
+vector<int> prime;
+
+int main()
+{
+	long int n;
+	cin >> n;
+	if (n == 1) {
+		cout << "1=1";
+		return 0;
+	}
+	prime.push_back(2);
+	for (int i = 3; i < 100000; i += 2) {
+		if (isprime[i]) {
+			prime.push_back(i);
+			for (int j = i * 2; j < 100000; j += i) {
+				isprime[j] = false;
+			}
+		}
+	}
+	int i = 0;
+	cout << n << "=";
+	bool first = true;
+	while (n > 1) {
+		int count = 0;
+		while (n > 1 && n % prime[i] == 0) {
+			n /= prime[i];
+			count++;
+		}
+		if (count > 0) {
+			if (first) {
+				first = false;
+				cout << prime[i];
+			}
+			else
+				cout << "*" << prime[i];
+			if (count > 1) {
+				cout << "^" << count;
+			}
+		}
+		i++;
+	}
+}
---
+++ 1060_Are_They_Equal/1060_Are_They_Equal/1060_Are_They_Equal.cpp
@@ -0,0 +1,53 @@
+﻿// 1060_Are_They_Equal.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+
+void f(string &s, int &u) {
+	u = 0;
+	int len = s.length();
+	while (u < len&&s[u] != '.') u++;
+	if (u == len) {
+		int t = u;
+		if (s == "0") u = 0;
+		for (int i = 0; i < n - t; i++) {
+			s.push_back('0');
+		}
+	}
+	else {
+		int w = 0;
+		while (w < len && (s[w] == '0' || s[w] == '.')) w++;
+		if (w == len) w = u;
+		s.erase(u, 1);
+		while (s[0] == '0') s.erase(0, 1);
+		len = s.length();
+		for (int i = 0; i < n - len; i++) {
+			s.push_back('0');
+		}
+		if (u < w) u = u - w + 1;
+		else u = u - w;
+	}
+	s = "0." + s.substr(0, n);
+
+}
+
+int main()
+{
+	string s1, s2;
+	cin >> n >> s1 >> s2;
+	int u1 = 0, u2 = 0;
+	f(s1, u1);
+	f(s2, u2);
+	if (u1 == u2 && s1 == s2) {
+		cout << "YES " << s1 << "*10^" << u1;
+	}
+	else {
+		cout << "NO " << s1 << "*10^" << u1 << " " << s2 << "*10^" << u2;
+	}
+}
